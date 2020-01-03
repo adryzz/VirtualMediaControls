f36763d2c08609d1783ba33afa4ea99b36db1aec
Adryzz <46694241+adryzz@users.noreply.github.com>
2020-01-03 15:12:35 +0100

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,257 @@
+﻿namespace VirtualMediaControls
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.enableVolumeControlToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.yesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.noToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.alwaysOnTopToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.yesToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.noToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.trackBar1.LargeChange = 10;
+            this.trackBar1.Location = new System.Drawing.Point(12, 122);
+            this.trackBar1.Maximum = 100;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(157, 45);
+            this.trackBar1.SmallChange = 5;
+            this.trackBar1.TabIndex = 3;
+            this.trackBar1.Scroll += new System.EventHandler(this.TrackBar1_Scroll);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.enableVolumeControlToolStripMenuItem,
+            this.alwaysOnTopToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(194, 70);
+            // 
+            // enableVolumeControlToolStripMenuItem
+            // 
+            this.enableVolumeControlToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.yesToolStripMenuItem,
+            this.noToolStripMenuItem});
+            this.enableVolumeControlToolStripMenuItem.Name = "enableVolumeControlToolStripMenuItem";
+            this.enableVolumeControlToolStripMenuItem.Size = new System.Drawing.Size(193, 22);
+            this.enableVolumeControlToolStripMenuItem.Text = "Enable volume control";
+            // 
+            // yesToolStripMenuItem
+            // 
+            this.yesToolStripMenuItem.Checked = true;
+            this.yesToolStripMenuItem.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.yesToolStripMenuItem.Name = "yesToolStripMenuItem";
+            this.yesToolStripMenuItem.Size = new System.Drawing.Size(91, 22);
+            this.yesToolStripMenuItem.Text = "Yes";
+            this.yesToolStripMenuItem.Click += new System.EventHandler(this.YesToolStripMenuItem_Click);
+            // 
+            // noToolStripMenuItem
+            // 
+            this.noToolStripMenuItem.Name = "noToolStripMenuItem";
+            this.noToolStripMenuItem.Size = new System.Drawing.Size(91, 22);
+            this.noToolStripMenuItem.Text = "No";
+            this.noToolStripMenuItem.Click += new System.EventHandler(this.NoToolStripMenuItem_Click);
+            // 
+            // alwaysOnTopToolStripMenuItem
+            // 
+            this.alwaysOnTopToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.yesToolStripMenuItem1,
+            this.noToolStripMenuItem1});
+            this.alwaysOnTopToolStripMenuItem.Name = "alwaysOnTopToolStripMenuItem";
+            this.alwaysOnTopToolStripMenuItem.Size = new System.Drawing.Size(193, 22);
+            this.alwaysOnTopToolStripMenuItem.Text = "Always on top";
+            // 
+            // yesToolStripMenuItem1
+            // 
+            this.yesToolStripMenuItem1.Checked = true;
+            this.yesToolStripMenuItem1.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.yesToolStripMenuItem1.Name = "yesToolStripMenuItem1";
+            this.yesToolStripMenuItem1.Size = new System.Drawing.Size(91, 22);
+            this.yesToolStripMenuItem1.Text = "Yes";
+            this.yesToolStripMenuItem1.Click += new System.EventHandler(this.YesToolStripMenuItem1_Click);
+            // 
+            // noToolStripMenuItem1
+            // 
+            this.noToolStripMenuItem1.Name = "noToolStripMenuItem1";
+            this.noToolStripMenuItem1.Size = new System.Drawing.Size(91, 22);
+            this.noToolStripMenuItem1.Text = "No";
+            this.noToolStripMenuItem1.Click += new System.EventHandler(this.NoToolStripMenuItem1_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(193, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.ExitToolStripMenuItem_Click);
+            // 
+            // button6
+            // 
+            this.button6.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.button6.Image = global::VirtualMediaControls.Properties.Resources.baseline_volume_up_black_48dp;
+            this.button6.Location = new System.Drawing.Point(120, 68);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(48, 48);
+            this.button6.TabIndex = 7;
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.Button6_Click);
+            // 
+            // button5
+            // 
+            this.button5.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.button5.Image = global::VirtualMediaControls.Properties.Resources.baseline_stop_black_48dp;
+            this.button5.Location = new System.Drawing.Point(66, 68);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(48, 48);
+            this.button5.TabIndex = 6;
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.Button5_Click);
+            // 
+            // button4
+            // 
+            this.button4.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.button4.Image = global::VirtualMediaControls.Properties.Resources.baseline_volume_off_black_48dp;
+            this.button4.Location = new System.Drawing.Point(11, 68);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(48, 48);
+            this.button4.TabIndex = 5;
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.Button4_Click);
+            // 
+            // button3
+            // 
+            this.button3.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.button3.Image = global::VirtualMediaControls.Properties.Resources.baseline_skip_next_black_48dp;
+            this.button3.Location = new System.Drawing.Point(120, 12);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(48, 48);
+            this.button3.TabIndex = 2;
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.Button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.button2.Image = global::VirtualMediaControls.Properties.Resources.baseline_play_arrow_black_48dp;
+            this.button2.Location = new System.Drawing.Point(66, 13);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(48, 48);
+            this.button2.TabIndex = 1;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.button1.Image = global::VirtualMediaControls.Properties.Resources.baseline_skip_previous_black_48dp;
+            this.button1.Location = new System.Drawing.Point(12, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(48, 48);
+            this.button1.TabIndex = 0;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.ContextMenuStrip = this.contextMenuStrip1;
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "VirtualMediaControls";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.NotifyIcon1_MouseDoubleClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(181, 170);
+            this.ContextMenuStrip = this.contextMenuStrip1;
+            this.ControlBox = false;
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Cursor = System.Windows.Forms.Cursors.Default;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.Text = "VirtualMediaControls";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem enableVolumeControlToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem yesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem noToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem alwaysOnTopToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem yesToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem noToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AudioSwitcher.AudioApi;
+using AudioSwitcher.AudioApi.CoreAudio;
+
+namespace VirtualMediaControls
+{
+    public partial class Form1 : Form
+    {
+        CoreAudioDevice device;
+        Point DefaultLocation;
+        public bool play = false;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            device = new CoreAudioController().DefaultPlaybackDevice;
+            trackBar1.Value = Convert.ToInt32(device.Volume);
+            DefaultLocation = Location;
+        }
+
+        #region drag
+        public const int WM_NCLBUTTONDOWN = 0xA1;
+        public const int HT_CAPTION = 0x2;
+
+        [DllImport("user32.dll")]
+        public static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
+        [DllImport("user32.dll")]
+        public static extern bool ReleaseCapture();
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                ReleaseCapture();
+                SendMessage(Handle, WM_NCLBUTTONDOWN, HT_CAPTION, 0);
+            }
+        }
+        #endregion
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            Previous();
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            play = !play;
+            if (play)
+            {
+                button2.Image = Properties.Resources.baseline_play_arrow_black_48dp;
+            }
+            else
+            {
+                button2.Image = Properties.Resources.baseline_pause_black_48dp;
+            }
+            PlayPause();
+        }
+
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            Next();
+        }
+
+        private void Button4_Click(object sender, EventArgs e)
+        {
+            Mute();
+        }
+
+        private void Button5_Click(object sender, EventArgs e)
+        {
+            Stop();
+        }
+
+        private void Button6_Click(object sender, EventArgs e)
+        {
+            UnMute();
+        }
+
+        #region keys
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern void keybd_event(byte virtualKey, byte scanCode, uint flags, IntPtr extraInfo);
+        public const int VK_MEDIA_NEXT_TRACK = 0xB0;
+        public const int VK_MEDIA_PLAY_PAUSE = 0xB3;
+        public const int VK_MEDIA_STOP = 0xB2;
+        public const int VK_MEDIA_PREV_TRACK = 0xB1;
+        
+        public const int KEYEVENTF_EXTENDEDKEY = 0x0001; //Key down flag
+        public const int KEYEVENTF_KEYUP = 0x0002; //Key up flag
+
+        private void PlayPause()
+        {
+            keybd_event(VK_MEDIA_PLAY_PAUSE, 0, KEYEVENTF_EXTENDEDKEY, IntPtr.Zero);
+            keybd_event(VK_MEDIA_PLAY_PAUSE, 0, KEYEVENTF_KEYUP, IntPtr.Zero);
+        }
+
+        private void Stop()
+        {
+            keybd_event(VK_MEDIA_STOP, 0, KEYEVENTF_EXTENDEDKEY, IntPtr.Zero);
+            keybd_event(VK_MEDIA_STOP, 0, KEYEVENTF_KEYUP, IntPtr.Zero);
+        }
+        private void Next()
+        {
+            keybd_event(VK_MEDIA_NEXT_TRACK, 0, KEYEVENTF_EXTENDEDKEY, IntPtr.Zero);
+            keybd_event(VK_MEDIA_NEXT_TRACK, 0, KEYEVENTF_KEYUP, IntPtr.Zero);
+        }
+
+        private void Previous()
+        {
+            keybd_event(VK_MEDIA_PREV_TRACK, 0, KEYEVENTF_EXTENDEDKEY, IntPtr.Zero);
+            keybd_event(VK_MEDIA_PREV_TRACK, 0, KEYEVENTF_KEYUP, IntPtr.Zero);
+        }
+        #endregion
+
+        #region volume
+        private void Mute()
+        {
+            device.Mute(true);
+        }
+
+        private void UnMute()
+        {
+            device.Mute(false);
+        }
+
+        private void Volume(int vol)
+        {
+            device.Volume = vol;
+        }
+        #endregion
+
+        private void TrackBar1_Scroll(object sender, EventArgs e)
+        {
+            Volume(trackBar1.Value);
+        }
+
+        protected override void OnClosing(CancelEventArgs e)
+        {
+            //close logic here
+            e.Cancel = true;
+            base.OnClosing(e);
+        }
+
+        #region menu
+
+        private void YesToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            yesToolStripMenuItem.Checked = true;
+            noToolStripMenuItem.Checked = false;
+            Height = 170;
+            trackBar1.Enabled = true;
+            trackBar1.Visible = true;
+        }
+
+        private void NoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            yesToolStripMenuItem.Checked = false;
+            noToolStripMenuItem.Checked = true;
+            Height = 129;
+            trackBar1.Enabled = false;
+            trackBar1.Visible = false;
+        }
+
+        private void YesToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            yesToolStripMenuItem1.Checked = true;
+            noToolStripMenuItem1.Checked = false;
+            TopMost = true;
+        }
+
+        private void NoToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            yesToolStripMenuItem1.Checked = false;
+            noToolStripMenuItem1.Checked = true;
+            TopMost = false;
+        }
+
+        private void ExitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void NotifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            Location = DefaultLocation;
+        }
+        #endregion
+    }
+}
